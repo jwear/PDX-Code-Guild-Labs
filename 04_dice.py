a0290279f806cde03da37101e63b73946520c926
Julie Anne Wear <jag68@icloud.com>
2017-09-29 16:35:25 -0700

    Revised dice lab

--- 04_dice.py
+++ 04_dice.py
@@ -1,10 +1,7 @@
 import random
-import os
-os.system('clear')
 
-num_dice = input("Let's roll some dice! How many dice do you want to roll?\n")
-num_sides = input("How many sides per die?\n")
+num_dice = input('How many die do you want to roll (limit 5)? ')
+num_sides = input('How many sides per die? ')
 
-print("Let's roll... ")
 for d in range(int(num_dice)):
     print(random.randrange(1, int(num_sides)))
