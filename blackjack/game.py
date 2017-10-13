618bf34745163aa42e09a1d4b72bf547be998756
Julie Anne Wear <jag68@icloud.com>
2017-10-13 16:37:21 -0700

    Added blackjack lab

---
+++ blackjack/game.py
@@ -0,0 +1,10 @@
+from card import Card
+from card import Deck
+from hand import Hand
+
+def user(user_game):
+    user_game = input('Would you like to start a new game - yes or no; or quit?').lower()
+    if user_game == 'yes':
+    
+    else:
+        quit()
