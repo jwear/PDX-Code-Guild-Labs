c3177965c18ed4fbbadab610a1425ca582c06dd8
Julie Anne Wear <jag68@icloud.com>
2017-12-04 16:20:59 -0800

    reorganized folders

--- javascript/greeting.js
+++
@@ -1,8 +0,0 @@
-var name = 'Julie Anne'
-var age = 35
-
-function nameAge(name, age) {
-  console.log("Hello! My name is " + name + " and I'm " + age + " years old.")
-}
-
-nameAge(name, age)
--- javascript/todos/todos.md
+++
@@ -1,6 +0,0 @@
-# What to do?
-
-Overview
-- Get the value from the text input
-- Create a li in our ul#todos
-- Add a remove and completed button on the list
